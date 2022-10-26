a2aa6abfad4b9fec5250030573069c97952d9288
blooren <blooren@outlook.com>
2022-10-25 22:19:26 -0300

    first server on express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { readFile } = require('fs');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    readFile('./home.html', 'utf8', (err, html) => {
+        if (err) {
+            response.status(500).send('sorry, servers down.')
+        }
+            response.send(html);
+    })
+    
+});
+
+app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
